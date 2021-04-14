9e8664239d9c3c9d0acbc00fa5c879316aebc544
HuYanChang <32992565+HuYanChang@users.noreply.github.com>
2021-04-14 16:19:02 +0800

    1

--- app/Controller/ConfigController.php
+++ app/Controller/ConfigController.php
@@ -4,25 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Hyperf\Config\Annotation\Value;
 use Hyperf\HttpServer\Annotation\Controller;
-use Hyperf\HttpServer\Annotation\GetMapping;
-use Hyperf\HttpServer\Contract\RequestInterface;
-use Hyperf\HttpServer\Contract\ResponseInterface;
 
 /**
  * @AutoController()
  */
 class ConfigController
 {
-    /**
-     * @Value("databases.default.driver")
-     */
-    private $config;
 
     public function index()
     {
         // 获取 autoload/databases.php 里的配置
-        return $this->config;
+        return 'AutoController';
     }
 }
