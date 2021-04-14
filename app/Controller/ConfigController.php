f9bdd802e0aa6c7064705c0aa1c5a7db7e9db54b
HuYanChang <32992565+HuYanChang@users.noreply.github.com>
2021-04-14 15:40:58 +0800

    '配置测试'

---
+++ app/Controller/ConfigController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @AutoController();
+ */
+namespace App\Controller;
+
+class ConfigController
+{
+
+    /**
+     * @Value(config.app_name)
+     */
+    private $appName;
+
+    public function index()
+    {
+        return $this->appName;
+    }
+}
