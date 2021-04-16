47532f518057088753d0788bc695d7f9928f20bf
HYC <13667272476@163.com>
2021-04-16 18:00:55 +0800

    sql

---
+++ migrations/2021_04_16_174204_create_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->uuid('uid')->comment('');
+           $table->tinyInteger('gender')->comment();
+            $table->tinyInteger('source')->comment();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+}
